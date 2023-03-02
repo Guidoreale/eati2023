9cbffb94d4055b45dfa2e599057b92672b8b6950
Guido <Guido@DESKTOP-2QGGVKP>
2023-03-02 14:15:08 -0300

    worflow 6 actualizacion. v3

---
+++ verificacion.py
@@ -0,0 +1,13 @@
+import subprocess
+
+def main():
+    resultado = subprocess.run(['.main'], capture_output=True)
+    if resultado.returncode == 0:
+        print("Éxito")
+    else:
+        print("Error")
+
+
+
+if __name__ == "__main__":
+    main()
